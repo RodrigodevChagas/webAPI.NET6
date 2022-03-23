27b19c725e4e737e8c7eb3ddbfd519f49aacc71f
Rodrigo Chagas <96203034+RodrigodevChagas@users.noreply.github.com>
2022-03-23 18:52:22 -0300

    Chama no brabo

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace SuperHeroes.Controllers
+{
+    //Criando a Controller
+
+    //nomeando a rota api
+    [Route("api/[controller]")]
+    //Declarando um atributo que melhora do desenvolvimento de APIs
+    [ApiController]
+    public class SuperHeroController : Controller
+    {
+        //Identifica uma ação que suporta o método GET
+        [HttpGet]
+     
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            //lista com as informações que serão passadas sobre os herois
+            var heroes = new List<SuperHero>
+            {
+                //instanciando a classe SuperHero
+                new SuperHero{
+                    Id = 1,
+                    Name = "Batman",
+                    FirstName = "Bruce",
+                    LastName = "Wayne",
+                    Place = "Gotham"
+                }
+            };
+            //status de retorno que sera produzido
+            return Ok(heroes);
+        }
+    }
+}
---
+++ SuperHero.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperHeroes
+{
+    public class SuperHero
+    {
+        //Criando Model, declarando quais dados serão passados e aplicando encapsulamento.
+        public int Id{ get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
